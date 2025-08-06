3adc36b06446f4a060462884d3a7ab043919c3fa
Nayeli De Jesus <nayelidjm142@gmail.com>
2025-08-05 21:32:32 -0700

    feat(auth): add login and signup functionality

--- journal/views.py
+++ journal/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
     return render(request, 'journal/signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'journal/login.html', {'form': form})
