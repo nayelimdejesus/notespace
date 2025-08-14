fb814dc1ce33d6d90a861418c4fcb99b62142fed
Nayeli De Jesus <nayelidjm142@gmail.com>
2025-08-14 15:43:58 -0700

    feat(templates): add base.html layout and integrate into existing templates

--- notespace/settings.py
+++ notespace/settings.py
@@ -61,7 +61,7 @@ LOGIN_REDIRECT_URL = '/journal/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
